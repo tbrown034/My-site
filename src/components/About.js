43c355c59efc47aa4dbe04db496fce3a4e721cef
tbrown034 <tbrown034@gmail.com>
2022-12-16 10:46:25 -0600

    Starting about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,35 @@
+import React from "react";
+import trevPhoto from "./Photos/trevorphoto3.jpeg";
+import Image from "react-bootstrap/Image";
+
 const About = () => {
-    return (
-        <>
-        <h1>Hello About</h1>
-        <div id="journalismExp">
+  return (
+    <>
+      <h1>A Little About Myslef</h1>
+      <div id="aboutContainter">
+        <div id="aboutWords">
+          <h3>
+            Hello! My name is Trevor, and I'm passionate about journalism,
+            telling important stories and web development.
+          </h3>
+          <br></br>
+          <h3>
+            I've been a reporter for nearly two decades, most recently working
+            as an investigative journalist for Oklahoma Watch.
+          </h3>
+          <br></br>
+          <h3></h3>
+          <h3>
+            I grew up in the suburbs of Chicago but now live in Oklahoma City
+            where I like to hike, explore new restaurants and breweries with
+            with my girlfriend, Lauren, and my dog, Indy.
+          </h3>
         </div>
-        </>
-    )
-}
+        <div id="aboutPic">
+        <Image src={trevPhoto} fluid id="secondPic" alt="Trevor again"></Image>
+        </div>
+      </div>
+    </>
+  );
+};
 export default About;
