8a4117c65776d63250ed6b1b8b28ed0b930d1912
tbrown034 <tbrown034@gmail.com>
2022-12-28 15:10:25 -0600

    changed tech stack and added more dataviz cards, working on tableau

--- src/components/DataProjects.js
+++ src/components/DataProjects.js
@@ -8,21 +8,18 @@ import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import trevPhoto from "./Photos/trevorPhoto2.jpeg";
 import Button from "@mui/material/Button";
-import ticTac from "./Photos/ticTac.png";
-import retrograde from "./Photos/Retrograde.png";
-import fitness from "./Photos/Fitness.png";
-import personal from "./Photos/Personal.png";
-import strangers from "./Photos/strangers.png";
+import chart2 from "./Photos/chart2.webp"
+
 
 {
-  /* <iframe title="In Three Days, Oklahoma Sees Number of Mass Shooting Since 2009 Increase from Five to Seven " aria-label="Map" id="datawrapper-chart-fMDc4" src="https://datawrapper.dwcdn.net/fMDc4/6/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="503" data-external="1"></iframe> */
+
 }
 
 export default function DataProjects() {
   return (
     <>
       <h2>Projects</h2>
-      <h4>
+      <h4>char
         In addition to the projects and web applications I built during my
         coding education, I enjoy exploring new technologies and finding new
         ways to grab, filter and display infomration.{" "}
@@ -31,35 +28,362 @@ export default function DataProjects() {
         <Card id="card" sx={{ maxWidth: 500 }}>
           <CardHeader
             avatar={<Avatar alt="Trevor" src={trevPhoto} />}
-            title="Personal Project"
-            subheader="Winter 2022"
+            title="Oklahoma Watch"
+            subheader="June 3, 2022"
             className="cardBackground"
           />
           <CardMedia className="dataEmbed"
           component="iframe"
           src="https://datawrapper.dwcdn.net/fMDc4/6/" 
-          scrolling="no" frameborder="1" 
-          
+          scrolling="yes" frameborder="1"
+          max-height="194"
+ 
         />
           <CardContent className="cardBackground">
-            <Typography paragraph>
-              Created this very website that you are now on using a frontend
-              tech stack that includes React, Bootstrap, Material UI.
-            </Typography>
+            
             <CardActions>
               <Button size="small">
                 {" "}
                 <a
-                  href="https://github.com/tbrown034/TicTacToeRedo.git"
+                  href="https://datawrapper.dwcdn.net/fMDc4/6/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  See the Code on Github
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2022/06/03/how-oklahoma-lawmakers-have-loosened-gun-regulations-the-past-decade%EF%BF%BC/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
                 </a>
               </Button>
             </CardActions>
           </CardContent>
         </Card>
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="April 18, 2022"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src="https://datawrapper.dwcdn.net/L0qvZ/4/" scrolling="yes" 
+          height="194"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://datawrapper.dwcdn.net/L0qvZ/4/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2022/06/03/how-oklahoma-lawmakers-have-loosened-gun-regulations-the-past-decade%EF%BF%BC/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="April 18, 2022"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src="https://datawrapper.dwcdn.net/NDN7G/9/" scrolling="yes" 
+          height="194"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://www.datawrapper.de/_/NDN7G/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2022/03/04/ice-holds-drop-as-immigrant-communities-law-enforcement-balance-policy-changes/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="Feb. 28, 2022"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src="https://datawrapper.dwcdn.net/m9I4h/2/"
+          scrolling="yes" 
+          height="194"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://www.datawrapper.de/_/m9I4h/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2022/02/28/claiming-out-of-state-influences-oklahoma-looks-to-clamp-down-on-state-question-laws/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="Feb. 28, 2022"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src="https://datawrapper.dwcdn.net/9FU1F/1/" 
+          scrolling="yes" 
+          height="194"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://www.datawrapper.de/_/9FU1F/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2021/12/20/as-some-oklahoma-churches-push-vaccines-others-sow-misinformation-doubt/?utm_content=bufferff609&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="Dec. 20, 2021"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src="https://datawrapper.dwcdn.net/eTHLz/1/"
+          scrolling="yes" 
+          height="194"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://www.datawrapper.de/_/eTHLz/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2022/01/24/oklahoma-legislative-financial-disclosures-tracking-potential-conflicts-of-interest/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="Dec. 20, 2021"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src="https://datawrapper.dwcdn.net/5Pout/1/"
+                    scrolling="yes" 
+          height="194"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://www.datawrapper.de/_/5Pout/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2021/10/21/redistricting-proposals-that-could-overhaul-oklahoma-politics/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="Dec. 20, 2021"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src="https://datawrapper.dwcdn.net/EncDs/4/"                    scrolling="yes" 
+          height="194"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://www.datawrapper.de/_/EncDs/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2022/06/02/hundreds-of-bills-passed-the-legislature-just-a-dozen-were-authored-by-democrats/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+
+        <Card id="card" sx={{ maxWidth: 500 }}>
+          <CardHeader
+            avatar={<Avatar alt="Trevor" src={trevPhoto} />}
+            title="Oklahoma Watch"
+            subheader="Dec. 20, 2021"
+            className="cardBackground"
+          />
+          <CardMedia className="dataEmbed"
+          component="iframe"
+          src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Bo&#47;BothMapsRedistricting&#47;Dashboard1&#47;1_rss.png'                    scrolling="yes" 
+          height="294"
+
+        />
+          <CardContent className="cardBackground">
+            
+            <CardActions>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://www.datawrapper.de/_/EncDs/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  See Full Visualization
+                </a>
+              </Button>
+              <Button size="small">
+                {" "}
+                <a
+                  href="https://oklahomawatch.org/2022/06/02/hundreds-of-bills-passed-the-legislature-just-a-dozen-were-authored-by-democrats/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read Related Article
+                </a>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+        
+        
       </div>
     </>
   );
--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,19 +1,18 @@
-import * as React from 'react';
-import Popover from '@mui/material/Popover';
-import Typography from '@mui/material/Typography';
-
+import * as React from "react";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
 
 export default function Tech() {
-   
-  
-    return (
+  return (
     <>
-    <h2>Tech Stack</h2>
+      <h2>Tech Stack</h2>
       <h4>
         My education focused on JavaScript web development training using the
         NERD stack (Node.js, Express, React, Databases using SQL.) Below are
         those and other technologies I have used.
       </h4>
+      <br></br>
+      <div id="deviconContainer">
       <div id="devicon">
         <i class="devicon-html5-plain colored"></i>
         <i class="devicon-css3-plain colored"></i>
@@ -21,12 +20,16 @@ export default function Tech() {
         <i class="devicon-react-original colored"></i>
         <i class="devicon-express-original colored"></i>
         <i class="devicon-nodejs-plain colored"></i>
-        <i class="devicon-postgresql-plain colored"></i>
-        <i class="devicon-bootstrap-plain colored"></i>
-        <i class="devicon-materialui-plain colored"></i>
-        <i class="devicon-vscode-plain colored"></i>
-        <i class="devicon-github-original colored"></i>
+        </div>
+        <div id="devicon">
+          <i class="devicon-postgresql-plain colored"></i>
+          <i class="devicon-bootstrap-plain colored"></i>
+          <i class="devicon-materialui-plain colored"></i>
+          <i class="devicon-vscode-plain colored"></i>
+          <i class="devicon-github-original colored"></i>
+        </div>
       </div>
+      <br></br>
     </>
   );
-};
+}
